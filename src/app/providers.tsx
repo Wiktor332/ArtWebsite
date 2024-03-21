110656bcb1dc6704e7689576fb4155049efac744
Wiktor Ćwikła <115033827+Wiktor332@users.noreply.github.com>
2024-03-21 20:07:06 +0100

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Navbar } from "@nextui-org/react";
 import {NextUIProvider} from "@nextui-org/system";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
