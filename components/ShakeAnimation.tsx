0e627f6ebebe6241e33f20185d46d7494bfea293
Wiktor332 <wiktor.stanislaw.cwikla@gmail.com>
2024-04-11 13:46:55 +0200

    control check 4

--- components/ShakeAnimation.tsx
+++ components/ShakeAnimation.tsx
@@ -8,7 +8,7 @@ const ShakeAnimation = () => {
   return (
     <a href="https://www.instagram.com/bambiogorek/" target='_blank' className={"block relative rounded-full overflow-hidden m-6"}>
       <Image
-        src="../../images/me/WhatsApp Image 2022-10-24 at 13.47.52.jpeg"
+        src="./images/me/WhatsApp Image 2022-10-24 at 13.47.52.jpeg"
         alt="me"
         width={500}
         height={500}
