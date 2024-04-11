1da3f6e25f3f7042bcfc5bffe45ecaa930856b97
Wiktor332 <wiktor.stanislaw.cwikla@gmail.com>
2024-04-11 12:35:29 +0200

    upd

--- artwebsite/components/ResponsiveContent.tsx
+++ artwebsite/components/ResponsiveContent.tsx
@@ -20,9 +20,9 @@ const ResponsiveContent = () => {
     <div className='flex flex-wrap'>
       {isLargeDevice ? (
         <>
-        <div className="flex flex-wrap space-evenly m-5 items-center">
+        <div className="flex flex-wrap space-evenly items-center ml-5">
           <ShakeAnimation />
-          <div className="flex flex-col font-mono p-4 w-1/2 h-auto font-semibold border-2 border-black">
+          <div className="flex flex-col ml-4 font-mono p-4 w-1/2 h-auto font-semibold border-2 border-black">
             <h1 className="text-3xl">KUTA KATARZYNA</h1>
             <p className="text-xl italic">
               Urodzona w 1985 roku w Puławach. Absolwentka Państwowego Liceum Sztuk Plastycznych w Nałęczowie. Studia w Instytucie Sztuk Pięknych UJK w Kielcach oraz na Wydziale Sztuki Uniwersytetu Rzeszowskiego. Dyplom (2011 r.) w pracowni rzeźby prof. UR Józefa Jerzego Kierskiego. W dorobku artystycznym udział w wystawach i plenerach w Polsce i za granicą. W swojej twórczości głównie inspiruje się naturą w której otoczeniu żyje.
