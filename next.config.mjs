9ba37a767ce0c22cc35b31efaa79a318bbb42d47
Wiktor332 <wiktor.stanislaw.cwikla@gmail.com>
2024-04-16 15:56:06 +0200

    upd: test

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,
     output: "export",
     images: { unoptimized: true },
 };
